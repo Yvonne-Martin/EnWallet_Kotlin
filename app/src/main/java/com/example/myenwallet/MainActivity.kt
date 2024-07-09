44b5330d5c12aa21dcc8b28e4e484317e1aaaaf1
Ynonne-Martin <yvonnemartinkatumbi@gmail.com>
2024-07-09 20:01:42 +0300

    adds files

--- app/src/main/java/com/example/myenwallet/MainActivity.kt
+++ app/src/main/java/com/example/myenwallet/MainActivity.kt
@@ -13,20 +13,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         binding.rvWallet.layoutManager=LinearLayoutManager(this)
          displayWallet()
     }
     fun displayWallet(){
-        val wallet1 = Wallet("2 June 2024","Kes 2344","kes 3444","")
-        val wallet2 = Wallet("2 May 2024","Kes 20944","kes 2344","")
-        val wallet3 = Wallet("2 June 2024","Kes 2344","kes 3444","")
-        val wallet4 = Wallet("2 May 2024","Kes 20944","kes 2344","")
-        val wallet5 = Wallet("2 June 2024","Kes 2344","kes 3444","")
-        val wallet6 = Wallet("2 May 2024","Kes 20944","kes 2344","")
-        val mywallets = listOf(wallet1,wallet2,wallet3,wallet4,wallet5,wallet6)
+        val wallet1 = Wallet("2 June 2024","Salary","kes 3444","")
+        val wallet2 = Wallet("2 May 2024","Rent","kes 2344","")
+        val wallet3 = Wallet("2 June 2024","Shopping","kes 3444","")
+        val wallet4 = Wallet("2 May 2024","Bus fare","kes 2344","")
+        val wallet5 = Wallet("2 June 2024","Water bill","kes 3444","")
+        val wallet6 = Wallet("2 May 2024","Electricity","kes 2344","")
+        val wallet7 = Wallet("2 May 2024","Groceries","kes 2344","")
+        val wallet8 = Wallet("2 June 2024","Food","kes 3444","")
+        val wallet9 = Wallet("2 May 2024","Bathings","kes 2344","")
+        val myWallets = listOf(wallet1,wallet2,wallet3,wallet4,wallet5,wallet6,wallet7,wallet8,wallet9)
 
-        val walletAdapter = WalletAdapter(mywallets)
+        val walletAdapter = WalletAdapter(myWallets)
         binding.rvWallet.adapter=walletAdapter
     }
 }
